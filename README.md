81a595a0c56aae808f7c612c10ea552477697110
francojc <francojc@wfu.edu>
2025-02-22 22:25:57 -0500

    build: Initial flake.nix, .envrc, .gitignore, and README.md files

---
+++ README.md
@@ -0,0 +1,1 @@
+TODO: create a repo that can use python to convert Gensim and FastText models to the Embedding Projector format 'vectors.tsv' 'metadata.tsv'
---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A Nix flake for creating a Python environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+
+      packages = with pkgs; [
+        pandoc
+        quarto
+      ];
+
+      # Python environment with packages
+      python = pkgs.python3.withPackages pythonPackages;
+
+      # Python packages
+      pythonPackages = ps:
+        with ps; [
+          ipython
+          jupyter
+        ];
+
+      allPackages = packages ++ [python];
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = allPackages;
+        shellHook = ''
+          # Add the current python to the path
+          export PATH=$python/bin:$PATH
+        '';
+      };
+    });
+}

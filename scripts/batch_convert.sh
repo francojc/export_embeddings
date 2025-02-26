6cd78a367165e7153443f0e8e10f83a793338e7f
francojc <francojc@wfu.edu>
2025-02-26 12:40:05 -0500

    feat: Add Icelandic language support and reorganize output directory

--- scripts/batch_convert.sh
+++ scripts/batch_convert.sh
@@ -10,10 +10,11 @@
 ./scripts/fasttext_convert.py ./models/Dutch/cc.nl.300.bin  --output_dir ./output/Dutch/ --limit 100000
 ./scripts/fasttext_convert.py ./models/Swedish/cc.sv.300.bin  --output_dir ./output/Swedish/ --limit 100000
 ./scripts/fasttext_convert.py ./models/Arabic/cc.ar.300.bin  --output_dir ./output/Arabic/ --limit 100000
+./scripts/fasttext_convert.py ./models/Icelandic/cc.is.300.bin  --output_dir ./output/Icelandic/ --limit 100000
 
 # GloVe models
-./scripts/glove_convert.py
+./scripts/glove_convert.py ./models/Twitter/glove.twitter.27B.200d.txt  --output_dir ./output/Twitter/ --limit 100000 --dimensions 200
 
 
 # Word2Vec models
-./scripts/gensim_convert.py
+./scripts/gensim_convert.py ./models/Icelandic/ICE/

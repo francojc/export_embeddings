707a50dcc60d8b79e5e7ab4e794f6b3a3ac05f0e
francojc <francojc@wfu.edu>
2025-02-26 11:47:10 -0500

    feat: Add batch download script and models directories file

--- scripts/batch_download.sh
+++ scripts/batch_download.sh
@@ -46,7 +46,7 @@ while IFS=$'\t' read -r URL TARGET_DIR FORCE; do
     fi
 
     # Execute download_model_files.sh for each URL and target directory
-    ./scripts/download_model_files.sh -h "$URL" -d "$TARGET_DIR" $FORCE_ARG
+    ./download_model_files.sh -h "$URL" -d "$TARGET_DIR" $FORCE_ARG
 
     echo "-------------------------"
 done < "$TSV_FILE"

f34a3cc2405e1745ebcac287a38b7442e96b9663
francojc <francojc@wfu.edu>
2025-02-27 11:20:37 -0500

    Feat: Add Icelandic embeddings from github and word2vec model.

--- scripts/batch_convert.sh
+++ scripts/batch_convert.sh
@@ -5,7 +5,7 @@
 # Usage: ./scripts/batch_convert.sh
 
 
-# FastText models
+# FastText models (cc stands for Common Crawl)
 ./scripts/fasttext_convert.py ./models/English/cc.en.300.bin  --output_dir ./output/English/ --limit 100000
 ./scripts/fasttext_convert.py ./models/Dutch/cc.nl.300.bin  --output_dir ./output/Dutch/ --limit 100000
 ./scripts/fasttext_convert.py ./models/Swedish/cc.sv.300.bin  --output_dir ./output/Swedish/ --limit 100000
@@ -15,5 +15,5 @@
 # GloVe models
 ./scripts/glove_convert.py ./models/Twitter/glove.twitter.27B.200d.txt  --output_dir ./output/Twitter/ --limit 100000 --dimensions 200
 
-# Word2Vec models
-# ./scripts/gensim_convert.py ./models/Icelandic/ICE/
+# Word2Vec models (ICE stands for Icelandic)
+./scripts/gensim_convert.py ./models/Icelandic/ICE/ICE_SG_300_20_15.model --output_dir ./output/Icelandic/ICE/ --limit 100000
